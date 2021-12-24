f69008ec6d75622de93d6ae1ee335924ecb97b98
Malik Abdulov <malik.abdulov@gmail.com>
2021-12-25 00:20:33 +0600

    hw_practice_week_fix

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,7 @@ import io.XlsReader;
 import model.Student;
 import model.University;
 import util.ComparatorUtil;
+import util.JsonUtil;
 
 import java.io.IOException;
 import java.util.List;
@@ -18,16 +19,40 @@ public class Main {
                 XlsReader.readXlsUniversities("src/main/resources/universityInfo.xlsx");
         UniversityComparator universityComparator =
                 ComparatorUtil.getUniversityComparator(UniversityComparatorType.YEAR);
-        universities.stream()
-                .sorted(universityComparator)
-                .forEach(System.out::println);
+        universities.sort(universityComparator);
+        String universitiesJson = JsonUtil.universityListToJson(universities);
+
+        System.out.println(universitiesJson);
+        List<University> universitiesFromJson = JsonUtil.jsonToUniversityList(universitiesJson);
+
+        System.out.println(universities.size() == universitiesFromJson.size());
+        universities.forEach(university -> {
+            String universityJson = JsonUtil.universityToJson(university);
+
+            System.out.println(universityJson);
+            University universityFromJson = JsonUtil.jsonToUniversity(universityJson);
+
+            System.out.println(universityFromJson);
+        });
 
         List<Student> students =
                 XlsReader.readXlsStudents("src/main/resources/universityInfo.xlsx");
         StudentComparator studentComparator =
                 ComparatorUtil.getStudentComparator(StudentComparatorType.AVG_EXAM_SCORE);
-        students.stream()
-                .sorted(studentComparator)
-                .forEach(System.out::println);
+        students.sort(studentComparator);
+        String studentsJson = JsonUtil.studentListToJson(students);
+
+        System.out.println(studentsJson);
+        List<Student> studentsFromJson = JsonUtil.jsonToStudentList(studentsJson);
+
+        System.out.println(students.size() == studentsFromJson.size());
+        students.forEach(student -> {
+            String studentJson = JsonUtil.studentToJson(student);
+
+            System.out.println(studentJson);
+            Student studentFromJson = JsonUtil.jsonToStudent(studentJson);
+
+            System.out.println(studentFromJson);
+        });
     }
 }
